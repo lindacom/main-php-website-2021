41e800330592e8c75ca8fa8dab6a801b363db9d7
lindacom <43495865+lindacom@users.noreply.github.com>
2018-09-22 13:55:16 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Web training website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="http://lindacom.infinityfreeapp.com/css/app.scss">
+   <link rel="stylesheet" type="text/css" href="css/modules.scss">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.css">
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/plugins/foundation.orbit.min.js"></script> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.1.1/motion-ui.min.css" />
+</head>
+  
+<body>
+            <!-- HERO SECTION -->
+
+  <section class="hero">
+
+           <div class="row intro">
+                <div class=" small-12 medium-6 large-6 columns">
+              <h1>Web development training website</h1>
+              <p >This website has been built using HTML, CSS, Javascript, PHP and SQL skills and is built on Foundation framework 6. 
+                  It documents continued learning about coaching and leadership. 
+                </p>
+    
+              <a href="http://lindacoach.epizy.com/drupal8/index.php" class="button primary">Coaching</a>
+               
+
+          </div>
+          </div>
+   </section>
+<!--- INTERRUPT -->
+ 
+   <section class="coloured">
+                                       
+                                  <div class="row intro">         
+
+                       <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+                            <h2>Projects</h2>
+                         </div>
+                       
+                        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+                                           <a href="http://lindacom.infinityfreeapp.com/blog/articles.php" class="button warning">Article listings</a><br />
+                                           <a href="http://lindacom.infinityfreeapp.com/forms.php" class="button warning">Forms</a><br />
+                                           <a href="http://lindacom.infinityfreeapp.com/books/library.php" class="button warning">Shopping cart</a><br />
+                          </div>
+                                       </div>
+  </section>
+          
+<!--- INTERRUPT -->
+ 
+          <section class="notcoloured">
+                                
+           <div class="row intro">
+
+                       <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+                            <h2>A website that I have been building</h2>
+                        </div>
+           <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+                     <a href="mainlandingpage.php" class="hollow button secondary">Code playground</a>
+
+               </div>
+</div>
+    </section>
+
+          <!-- MAIN SECTION -->
+
+   
+
+
+ <section class="coloured">
+<div class="wrap row">
+
+    <div class="grid-container">
+          <div class="grid-x grid-margin-x">
+
+<!--- THREE COLUMN SECTION-->
+               <div class="row"style="padding-bottom: 40px;">
+                           
+                            <div class="col-xs-12 col-sm-3 col-md-3">
+                              <i class="fas fa-code"></i> 
+                              <h3>HTML5</h3>  
+                               <p>Using HTML to build the website</p> 
+                            </div> 
+
+                           <div class="col-xs-12 col-sm-3 col-md-3">
+                                <i class="fas fa-bold"></i>
+                                 <h3>BOOTSTRAP</h3>
+                                 <p>Using CSS, Bootstrap and Foundation framework to design the website</p>
+                            </div>
+
+                            <div class="col-sm-12 col-sm-3 col-md-3">
+<i class="fab fa-css3"></i>
+<h3>CODING </h3>
+<p>Using SQL databases, JavaScript and PHP to make the website more interactive</p>
+</div>
+
+<div class="col-sm-12 col-sm-3 col-md-3">                      
+<i class="fab fa-drupal"></i>                      
+<h3>CMS </h3>
+<p>Using Drupal content management system to build the coaching microsite</p>
+</div>
+
+            </div>
+      </div> 
+      </div>
+      </div> 
+                        </section> 
+            
+               
+                   
+
+
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+<script src="js/foundation/foundation.js"></script>
+  <script src="js/foundation/foundation.offcanvas.js"></script>
+ <script> $(document).foundation();</script>
+ </body> 
+</html>
