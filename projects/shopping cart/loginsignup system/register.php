5e126ba48e96e7511ffdd38b801f16c32fc3f2cb
lindacom <43495865+lindacom@users.noreply.github.com>
2021-01-20 13:21:00 +0000

    Update and rename projects/shopping cart/register.php to projects/shopping cart/loginsignup system/register.php

--- projects/shopping cart/loginsignup system/register.php
+++ projects/shopping cart/loginsignup system/register.php
@@ -1,9 +1,3 @@
-<?php 
- // displays errors in the page for testing purposes
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-?> 
 <?php
 // session_start();
 
